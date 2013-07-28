2f97ee0682a3c0686441d12a80bd8ead6c380b20
Jim McAndrew <jim@loc8.us>
2013-07-28 14:25:02 -0600

    Changed how modes work, added a function to return json from an external source

--- rest.js
+++ rest.js
@@ -1,36 +1,79 @@
+var renderResult = function (req, res, jsonResult, title) {
+
+    // Functions 
+    // ///////////////////////////////
+    // Read Params here
+    var getParams = function(req) {
+        // Deal with any params after a question mark
+        var newParamsRaw = req._parsedUrl.query ? req._parsedUrl.query.split("&") : [];
+        var newParams = {};
+        newParamsRaw.map(function(params) {
+            var param = params.split(/=(.*)/, 2);
+            if (param.length === 2) {
+                newParams[param[0].toLowerCase()] = param[1];
+            }
+        });
+        return newParams;
+    };
+    // Convert output to jsonp if required
+    var toJsonp = function(json) {
+        if (queryParams.callback) {
+            return queryParams.callback + "(" + json + ");";
+        } else {
+            return json;
+        }
+    };
+    //////////////////////////////////
+
+    // Read the Params
+    var queryParams = getParams(req);
+
+    // Pretty print output if required
+    var indent = queryParams.pretty ? 4 : null;
+    // Run parse and stringify (possibly again), this will format it with the indent as well as escape anything weird
+    var jsonString = JSON.stringify(JSON.parse(jsonResult), null, indent);
+
+    if (req.params && req.params.format === "json" || req.params.format === "jsonp") {
+        // Write out the JSON to the user
+        res.writeHead(200, {'Content-Type' : 'application/json' });
+        res.write(toJsonp(jsonString));
+        res.end();
+    } else {
+        // Return as HTML
+
+        // Clean up the object before sending it out
+        var returnedObject = JSON.parse(jsonString);
+        if (!(returnedObject instanceof Array)) {returnedObject = [returnedObject];}
+        res.render('index', { "title": title, "data": returnedObject});
+    }
+};
+
 exports.addService = function(app, table, mode) {
     // return the data in a useful format
     var formatData = function(documents, req, res, returnFirstOnly) {
-        // Deal with any params after a question mark
-        var queryParamsRaw = req._parsedUrl.query ? req._parsedUrl.query.split("&") : [];
-        var queryParams = {};
-        queryParamsRaw.map(function(params) {
-            var param = params.split(/=(.*)/, 2);
-            if (param.length === 2) {
-                queryParams[param[0].toLowerCase()] = param[1];
-            }
-        });
 
         // Deal with returns of single items
         if (returnFirstOnly && documents) {
             documents = documents[0];
-        };
+        }
 
         // Deal with undefined documents
         documents = documents ? documents : [];
-        
-        // Filter the output based on user settings
-        var filter = null;
-        if (table.displayFields && (table.displayFields[mode] || table.displayFields[table.allUsersParam])) {
-            filter = [];
-            table.displayFields[mode].map(function(field){filter.push(field);});
-            table.displayFields[table.allUsersParam].map(function(field){filter.push(field);});
-        }
 
-        var getJsonString = function(currDocuments, currFilter, currIndent){
+        // Filter the output based on user settings
+        var filter = [];
+        table.displayFields.map(function(field) {
+            field.permissions.map(function(newPermission) {
+                if (newPermission == mode(req) || newPermission == table.allUsersParam) {
+                    filter.push(field.name);
+                }
+            });
+        });
+
+        var getJsonString = function(currDocuments, currFilter){
             var returnValue;
             var convertId = function(record) {
-                record["id"] = record._id;
+                record.id = record._id;
                 delete record._id;
 
                 if (currFilter.indexOf("_id") > -1) {
@@ -40,55 +83,41 @@ exports.addService = function(app, table, mode) {
 
             if (currDocuments instanceof Array) {
                 currDocuments.map(convertId);
-                returnValue = JSON.stringify(currDocuments.map(function(output){return output;}), currFilter , currIndent);
+                returnValue = JSON.stringify(currDocuments.map(function(output){return output;}), currFilter);
             } else {
                 convertId(currDocuments);
-                returnValue = JSON.stringify(currDocuments, currFilter , currIndent);
+                returnValue = JSON.stringify(currDocuments, currFilter);
             }
 
             return returnValue;
         };
 
-        // Pretty print output if required
-        var indent = queryParams.pretty ? 4 : null;
+        var jsonResult = getJsonString(documents, filter);
+        var pageTitle = table.model.modelName;
 
-        if (req.params && req.params.format === "json" || req.params.format === "jsonp") {
+        // Render the Result to the Browser
+        renderResult(req, res, jsonResult, pageTitle);
+    };
 
-            // Convert output to jsonp if required
-            var toJsonp = function(json) {
-                if (queryParams.callback) {
-                    return queryParams.callback + "(" + json + ");";
-                } else {
-                    return json;
-                }
-            };
-
-            // Write out the JSON to the user
-            res.writeHead(200, {'Content-Type' : 'application/json' });
-            res.write(toJsonp(getJsonString(documents, filter, indent)));
-            res.end();
+    var checkPermission = function(req, res, level, method, callback) {
+        if (level.crud[mode(req)][method]) {
+            callback();
         } else {
-            // Return as HTML
-
-            // Clean up the object before sending it out
-            var returnedObject = JSON.parse(getJsonString(documents, filter, indent));
-            if (!(returnedObject instanceof Array)) {returnedObject = [returnedObject];}
-            res.render('index', { "title": table.model.modelName, "data": returnedObject});
+            res.send(401, "Unauthorized");
         }
     };
 
     // List All
-    if (table.crud[mode].r) {
-        app.get('/' + table.model.modelName + '.:format', function(req, res) {
+    app.get('/' + table.model.modelName + '.:format', function(req, res) {
+        checkPermission(req, res, table, "r", function(){
             table.model.find().lean().exec(function (err, documents) {
                 formatData(documents, req, res, false);
             });
         });
-    }
-
+    });
     // Create
-    if (table.crud[mode].c) {
-        app.post('/' + table.model.modelName + '.:format?', function(req, res) {
+    app.post('/' + table.model.modelName + '.:format?', function(req, res) {
+        checkPermission(req, res, table, "c", function(){
             var newDocument = new table.model(req.body);
             newDocument.save(function() {
                 table.model.findById(newDocument._id).lean().exec(function (err, document) {
@@ -96,7 +125,7 @@ exports.addService = function(app, table, mode) {
                 });
             });
         });
-    }
+    });
 
     var createQuery = function(thisField) {
         // Internal Functions
@@ -118,7 +147,6 @@ exports.addService = function(app, table, mode) {
 
        // Update Function
        var updateCurrentData = function (req, res) {
-           console.log("UPDATE");
            table.model.find(queryFunction(req.params.field), function (err, documents) {
                if (documents && documents.length > 0) {
                    var callbackCount = 0;
@@ -176,19 +204,21 @@ exports.addService = function(app, table, mode) {
             "DELETE": deleteCurrentData
         };
         // Add the REST methods
-        for (method in thisField.crud[mode]) {
-            if (table.crud[mode][method] && method != "c") {
-                app[crudLookup[method]]('/' + table.model.modelName + '/' + thisField.name + '/' + ':field.:format?', function(req, res){
+        for (var method in crudLookup) {
+            app[crudLookup[method]]('/' + table.model.modelName + '/' + thisField.name + '/' + ':field.:format?', function(req, res){
+                checkPermission(req, res, thisField, method, function(){
                     reqLookup[req.method](req, res);
                 });
+            });
 
-                // Check if this is the default method, and add the defaults as well
-                if (table.defaultField === thisField.dbField) {
-                    app[crudLookup[method]]('/' + table.model.modelName + '/' + ':field.:format?', function(req, res) {
+            // Check if this is the default method, and add the defaults as well
+            if (table.defaultField === thisField.dbField) {
+                app[crudLookup[method]]('/' + table.model.modelName + '/' + ':field.:format?', function(req, res) {
+                    checkPermission(req, res, thisField, method, function(){
                         req.returnOne = true;
                         reqLookup[req.method](req, res);
                     });
-                };
+                });
             }
         }
     };
@@ -198,5 +228,14 @@ exports.addService = function(app, table, mode) {
     table.queryFields.map(function(queryField) {
         //crudFields(queryField);
         createQuery(queryField);
+    });
+};
+
+exports.returnJson = function(app, jsonOutput, path, mode) {
+
+    app.get(path + ".:format?", function(req, res) {
+        // TODO: modes on this too?
+        // Return the JSON to the requestor
+        renderResult(req, res, jsonOutput, "Request: " + path);
     });
 };
